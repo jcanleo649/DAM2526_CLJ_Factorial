3cab90f54528dbd413bcf2d093db4317d7951797
jcanleon649 <jcanleon649@g.educaand.es>
2025-12-10 09:08:03 +0100

    Segunda Versión sin bin, ya que antes me equivoque

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -1,9 +1,25 @@
 package pruebaControlVersiones;
 
+import java.util.Scanner;
+
 public class Factorial {
-
+	private static Scanner sc = new Scanner(System.in);
+	 
 	public static void main(String[] args) {
+		
+		int num = pedirNumero();
 
+        if (num < 0) {
+            System.out.println("ERROR: el número debe ser mayor o igual que 0.");
+        } else {
+            System.out.println("Has introducido el número: " + num);
+        }
+
+        sc.close();
+	}
+	private static int pedirNumero() {
+	    System.out.print("Introduce un número entero mayor o igual que 0: ");
+	    return sc.nextInt();
 	}
 
 }
